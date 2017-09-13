3ac7c261f3db146e7ecdc0c4f61feb2f7e6d174b
yqxia <yqxia@thoughtworks.com>
2017-09-13 23:05:46 +0800

    Welcome to Bibliteca when is customer

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,25 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp app = new BibliotecaApp();
+
+        System.out.println("please input your name:");
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+
+        app.biblioteca(str);
     }
+
+    void biblioteca(String str) {
+
+        if (str.equals("customer")) {
+            System.out.print("Welcome to Biblioteca!");
+        }
+    }
+
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,17 @@
 package com.twu.biblioteca;
 
-
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void customerGetWelcomeMassage() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        new BibliotecaApp().biblioteca("customer");
+        assertEquals(output.toString(),"Welcome to Biblioteca!");
     }
 }
